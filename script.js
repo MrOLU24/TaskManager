e66be2964da6ee5fd32265e734e2fbd519e39ad7
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-06 16:06:41 +0100

    getting value from input

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const TaskManager = [];
 document.getElementById("TaskForm").addEventListener("submit", (e) => {
   e.preventDefault();
-  let Todo = document.getElementById("taskone").value;
-  TaskManager.push({ Todo: Todo, Status: "Completed" });
+  let Todo = document.getElementById("AddTask").value;
+  TaskManager.push({ Todo: Todo, status: "pending" });
 });
 console.log(TaskManager);
+
+const DisplayContent = () => {
+  const element = (document.getElementById("content").innerHTML = `
+   <div class="card border-0 mx-5 shadow-sm mt-3">
+        <div class="card-body">
+          <h2 class="text-capitalize">Task Title</h2>
+          <button class="btn">
+            <i class="fa-regular fa-pen-to-square"></i>Edit
+          </button>
+          <button class="btn"><i class="fa-solid fa-trash"></i>Delete</button>
+        </div>
+      </div>
+
+`);
+};
