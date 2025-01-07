43385993d313382493d6537aa5fc25cda80a9735
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-07 21:00:09 +0100

    still working on js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const TaskManager = [];
+const TaskManager = [{Todo: "working", status: "completed"}, {Todo: "coding", status: "incomplete"}];
 document.getElementById("TaskForm").addEventListener("submit", (e) => {
   e.preventDefault();
   let Todo = document.getElementById("AddTask").value;
@@ -6,17 +6,23 @@ document.getElementById("TaskForm").addEventListener("submit", (e) => {
 });
 console.log(TaskManager);
 
-const DisplayContent = () => {
-  const element = (document.getElementById("content").innerHTML = `
-   <div class="card border-0 mx-5 shadow-sm mt-3">
-        <div class="card-body">
-          <h2 class="text-capitalize">Task Title</h2>
-          <button class="btn">
-            <i class="fa-regular fa-pen-to-square"></i>Edit
-          </button>
-          <button class="btn"><i class="fa-solid fa-trash"></i>Delete</button>
-        </div>
-      </div>
-
-`);
+const DisplayContent = (data) => {
+  const element = document.getElementById("content");
+  element.innerHTML = "";
+  data.map((val,key)=>{
+    element.innerHTML += `
+    <div class="card border-0 mx-5 shadow-sm mt-3">
+         <div class="card-body">
+           <h2 class="text-capitalize">${val.Todo}</h2>
+           <button class="btn">
+             <i class="fa-regular fa-pen-to-square"></i>Edit
+           </button>
+           <button class="btn"><i class="fa-solid fa-trash"></i>Delete</button>
+           <span class="badge float-end bg-secondary">pending</span
+         </div>
+       </div>
+ 
+ `;
+  })
 };
+DisplayContent(TaskManager)
