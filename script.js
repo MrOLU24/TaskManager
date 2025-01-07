391469b4adcdd1eb3077efa3c5eb21582701abda
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-07 22:10:29 +0100

    on to the next

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-const TaskManager = [{Todo: "working", status: "completed"}, {Todo: "coding", status: "incomplete"}];
+const TaskManager = [
+  { Todo: "working", status: "completed" },
+  { Todo: "coding", status: "incomplete" },
+];
 document.getElementById("TaskForm").addEventListener("submit", (e) => {
   e.preventDefault();
   let Todo = document.getElementById("AddTask").value;
   TaskManager.push({ Todo: Todo, status: "pending" });
+  DisplayContent(TaskManager);
 });
 console.log(TaskManager);
 
 const DisplayContent = (data) => {
   const element = document.getElementById("content");
   element.innerHTML = "";
-  data.map((val,key)=>{
+  data.map((val, key) => {
     element.innerHTML += `
     <div class="card border-0 mx-5 shadow-sm mt-3">
          <div class="card-body">
            <h2 class="text-capitalize">${val.Todo}</h2>
-           <button class="btn">
+           <button class="btn" onclick"(${key})">
              <i class="fa-regular fa-pen-to-square"></i>Edit
            </button>
            <button class="btn"><i class="fa-solid fa-trash"></i>Delete</button>
-           <span class="badge float-end bg-secondary">pending</span
+           <span class="badge float-end bg-secondary ${
+             val.status === "pending"
+               ? "bg-warning"
+               : val.status === "completed"
+               ? "bg-success"
+               : "bg-danger"
+           }">
+             ${val.status}
+             </span>
          </div>
        </div>
- 
  `;
-  })
+  });
 };
-DisplayContent(TaskManager)
+DisplayContent(TaskManager);
