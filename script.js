049435df5dec7ddbb4b14fe8745826f956fa056c
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-08 12:46:27 +0100

    working on local storage

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const TaskManager = [
-  { Todo: "working", status: "completed" },
-  { Todo: "coding", status: "incomplete" },
-];
+const TaskManager = [];
 document.getElementById("TaskForm").addEventListener("submit", (e) => {
   e.preventDefault();
   let Todo = document.getElementById("AddTask").value;
@@ -19,10 +16,10 @@ const DisplayContent = (data) => {
     <div class="card border-0 mx-5 shadow-sm mt-3">
          <div class="card-body">
            <h2 class="text-capitalize">${val.Todo}</h2>
-           <button class="btn" onClick="Edit(3)">
+           <button class="btn" onClick="Edit(${key})">
              <i class="fa-regular fa-pen-to-square"></i>Edit
            </button>
-           <button class="btn"><i class="fa-solid fa-trash"></i>Delete</button>
+           <button class="btn" onClick="Delete(${key})"><i class="fa-solid fa-trash"></i>Delete</button>
            <span class="badge float-end bg-secondary ${
              val.status === "pending"
                ? "bg-warning"
@@ -45,10 +42,24 @@ const Edit = (id) => {
   EditModal.show();
   document.getElementById("EditTask").value = TaskManager[id].Todo;
   document.getElementById("EditStatus").value = TaskManager[id].Todo;
-  document.getElementById('TaskId').value = id
+  document.getElementById("TaskId").value = id;
 };
 
 //save
-document.getElementById('EditForm').addEventListener('submit', (e)=>{
-  e.preventDefault()
-})
+document.getElementById("EditForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let editTask = document.getElementById("EditTask").value;
+  let status = document.getElementById("EditStatus").value;
+  let taskId = document.getElementById("TaskId").value;
+  TaskManager[taskId].Todo = editTask;
+  TaskManager[taskId].status = status;
+  DisplayContent(TaskManager);
+  EditModal.hide();
+});
+//delete
+
+const Delete = (TaskId) => {
+  if (confirm("are you sure you want to delete?"))
+    TaskManager.splice(TaskId, 1);
+  DisplayContent(TaskManager);
+};
