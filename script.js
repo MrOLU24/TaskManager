91072d81d911ee19e322c02812a862ee80b64dce
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-08 11:44:09 +0100

    working on save Task

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const TaskManager = [
 document.getElementById("TaskForm").addEventListener("submit", (e) => {
   e.preventDefault();
   let Todo = document.getElementById("AddTask").value;
-  TaskManager.push({ Todo: Todo, status: "pending" });
+  TaskManager.push({ Todo: Todo, status: "Incomplete" });
   DisplayContent(TaskManager);
-  AddModal.hide()
+  AddModal.hide();
 });
 console.log(TaskManager);
 
@@ -19,7 +19,7 @@ const DisplayContent = (data) => {
     <div class="card border-0 mx-5 shadow-sm mt-3">
          <div class="card-body">
            <h2 class="text-capitalize">${val.Todo}</h2>
-           <button class="btn" onclick"(${key})">
+           <button class="btn" onClick="Edit(3)">
              <i class="fa-regular fa-pen-to-square"></i>Edit
            </button>
            <button class="btn"><i class="fa-solid fa-trash"></i>Delete</button>
@@ -38,4 +38,17 @@ const DisplayContent = (data) => {
   });
 };
 DisplayContent(TaskManager);
-var AddModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+var AddModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+var EditModal = new bootstrap.Modal(document.getElementById("Editmodal"));
+const Edit = (id) => {
+  // alert(id)
+  EditModal.show();
+  document.getElementById("EditTask").value = TaskManager[id].Todo;
+  document.getElementById("EditStatus").value = TaskManager[id].Todo;
+  document.getElementById('TaskId').value = id
+};
+
+//save
+document.getElementById('EditForm').addEventListener('submit', (e)=>{
+  e.preventDefault()
+})
